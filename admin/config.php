a349638ebc985b72a2f6bdaa2e391fc04486486d
cheerluck <1078619622@qq.com>
2019-03-28 19:36:38 +0800

    首次上传代码

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test:8080/chucloud2019/3C/admin/');
+define('HTTP_CATALOG', 'http://test:8080/chucloud2019/3C/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test:8080/chucloud2019/3C/admin/');
+define('HTTPS_CATALOG', 'http://test:8080/chucloud2019/3C/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/xampp/htdocs/demo/chucloud2019/3C/admin/');
+define('DIR_SYSTEM', 'E:/xampp/htdocs/demo/chucloud2019/3C/system/');
+define('DIR_IMAGE', 'E:/xampp/htdocs/demo/chucloud2019/3C/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'E:/xampp/htdocs/demo/chucloud2019/3C/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mc_');
+
+// MyCnCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ admin/language/zh-hk/catalog/series.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']				= '商品系列';
+
+// Text
+$_['text_success']			  	= '成功：您已修改商品系列！';
+$_['text_list']			  		= '商品系列清單';
+$_['text_add']			  		= '新增商品系列';
+$_['text_edit']			 		= '編輯商品系列';
+$_['text_default']				= '預設';
+$_['text_keyword']				= '請勿使用空格，使用 - 取代空格，並確定靜態網址(SEO URL)是沒有重複過的。';
+
+// Column
+$_['column_name']				= '系列名稱';
+$_['column_sort_order']			= '系列排序';
+$_['column_action']				= '編輯系列';
+	
+// Entry
+$_['entry_name']				= '系列名稱';
+$_['entry_description']			= '系列描述';
+$_['entry_meta_title'] 			= 'Meta 標籤標題';
+$_['entry_meta_keyword']		= 'Meta 標籤關鍵字';
+$_['entry_meta_description']	= 'Meta 標籤描述';
+$_['entry_store']				= '商店';
+$_['entry_keyword']				= '靜態網址(SEO URL)';
+$_['entry_parent']				= '系列層級';
+$_['entry_filter']				= '商品過濾';
+$_['entry_image']				= '系列圖片';
+$_['entry_top']					= '導覽列顯示';
+$_['entry_column']				= '列排顯示';
+$_['entry_sort_order']			= '系列排序';
+$_['entry_status']				= '系列狀態';
+$_['entry_layout']				= '佈局覆蓋';
+
+// Help
+$_['help_filter']				= '(自動完成)';
+$_['help_top']					= '在頁面上方導覽列顯示，僅適用頂層系列。';
+$_['help_column']				= '在頁面上方主系列下的子系列顯示時排列的行數。 僅適用頂層系列。';
+
+// Error 
+$_['error_warning']				= '警告：相關欄位資料未輸入！';
+$_['error_permission']			= '警告：您沒有權限更改商品系列！';
+$_['error_name']				= '系列名稱必須是 1 到 255 個字！';
+$_['error_meta_title']			= 'Meta 標題必須是 1 到 255 個字！';
+$_['error_keyword']				= '靜態網址(SEO URL)已使用中!';
+$_['error_unique']				= '靜態網址(SEO URL)必須是沒有重複的!';
+$_['error_parent']           	= '您所選擇的系列層級不能是此系列的子系列!';
+
